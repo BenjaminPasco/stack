9d1556fc11f9323c249253b80dee990948b5a20c
bpasco <benjamin.pasco@hotmail.fr>
2024-09-13 23:11:31 +0200

    deps, build: add vite-tsconfig-paths to vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { vitePlugin } from "@remix-run/dev";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [vitePlugin()],
+  plugins: [vitePlugin(), tsconfigPaths()],
 });
