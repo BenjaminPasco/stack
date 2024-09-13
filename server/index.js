d78657550f687b419202dbb0fb2460c4335c85f7
bpasco <benjamin.pasco@hotmail.fr>
2024-09-14 00:05:03 +0200

    add: fastify server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import process from "node:process";
+import { remixFastify } from "@mcansh/remix-fastify";
+import { installGlobals } from "@remix-run/node";
+import { fastify } from "fastify";
+import sourceMapSupport from "source-map-support";
+import getPort, { portNumbers } from "get-port";
+installGlobals();
+sourceMapSupport.install();
+const app = fastify();
+await app.register(remixFastify);
+const host = process.env.HOST === "true" ? "0.0.0.0" : "127.0.0.1";
+const desiredPort = Number(process.env.PORT) || 3000;
+const portToUse = await getPort({
+  port: portNumbers(desiredPort, desiredPort + 100),
+});
+let address = await app.listen({ port: portToUse, host });
+if (portToUse !== desiredPort) {
+  console.warn(
+    `Port ${desiredPort} is not available, using ${portToUse} instead.`
+  );
+}
+console.log(`app ready: ${address}`);
